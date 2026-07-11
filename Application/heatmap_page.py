407c7e1137cb9227cf906f38669660d0f4850ff0
dulnith-liyanage <dnethmiraliyanage@gmail.com>
2026-07-11 19:23:16 +0530

    Corrected heatmap

--- Application/heatmap_page.py
+++ Application/heatmap_page.py
@@ -3,6 +3,8 @@ import pandas as pd
 import geopandas as gpd
 import matplotlib.pylab as plt
 
+st.sidebar.markdown("*This heat map represents the districtwise distribution of the Total Price*")
+
 if "dataset" in st.session_state:
     df = st.session_state['dataset']
     st.sidebar.markdown("*Currently using uploaded file.*")
@@ -33,25 +35,30 @@ geo_data = geo_data[['ADM2_EN', 'geometry']].rename(columns={'ADM2_EN': 'Distric
 
 dis_df = df.groupby('District')['Total_Price_LKR'].sum().sort_values(ascending=False).reset_index()
 district = geo_data.merge(dis_df, how='left', left_on='District', right_on='District')
-
-st.markdown("### Districtwise Distribution of the Total Price")
-st.sidebar.markdown("*This heat map represents the districtwise distribution of the Total Price*")
-
 district = district.iloc[1:]
-fig, ax = plt.subplots(figsize=(10, 8))
+
 district["Total_Price_LKR"] = district["Total_Price_LKR"]/1000000  # Convert to millions
 
-# for spine in ax.spines.values():
-#     spine.set_color('white') 
-# ax.tick_params(axis='both', colors='white', labelsize=10)
-ax.axis('off')
+district = district[district['Total_Price_LKR'] > 0] # Remove rows with zero Total_Price_LKR values
 
-district = district.sort_values(by='Total_Price_LKR', ascending=True)
-district.plot(column='Total_Price_LKR', cmap='Purples', legend=True, ax=ax, color='white')
-
-st.pyplot(fig, clear_figure=True, transparent=True)
-
+st.markdown("## Districtwise Distribution of the Total Price")
+st.write("")
+st.markdown("#### Bar Chart Representation")
 district_data = df.groupby('District')['Total_Price_LKR'].sum().reset_index()
 district_data['Total_Price_LKR'] = district_data['Total_Price_LKR'] / 1000000
 
-my_chart = st.bar_chart(district_data, x='District', y='Total_Price_LKR', x_label='District', y_label='Total Price in Million LKR', color="#C57AF4")
+# Create a bar chart
+st.bar_chart(district_data, x='District', y='Total_Price_LKR',
+              x_label='District'
+             , y_label='Total Price in Million LKR', color="#8aadf4")
+
+st.markdown("#### Heatmap Representation")
+# Create a choropleth map
+fig, ax = plt.subplots(figsize=(10, 8))
+
+ax.axis('off')
+
+district = district.sort_values(by='Total_Price_LKR', ascending=True)
+district.plot(column='Total_Price_LKR', cmap="Blues", legend=True, ax=ax, color='white')
+
+st.pyplot(fig, clear_figure=True, transparent=True)
