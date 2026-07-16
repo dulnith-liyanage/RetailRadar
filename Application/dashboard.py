63e3797dbf0df411ec2aa9d43535e479f5b56662
Thenul <sahant2005@gmail.com>
2026-07-16 12:35:10 +0530

    Added the chapbot page

--- Application/dashboard.py
+++ Application/dashboard.py
@@ -4,6 +4,6 @@ welcome_page = st.Page("welcome.py", title="Welcome")
 sales_performance_page = st.Page("sales_performance.py", title="Sales Performance", icon="📈")
 district_distribution_page = st.Page("district_distribution.py", title="Districtwise Distribution", icon="📌")
 rfm_page = st.Page("rfm.py", title="Customer Segments", icon="👥")
-
+chatbot_page = st.Page("bot.py", title = "Insight.AI", icon="🤖")
 pg = st.navigation([welcome_page, sales_performance_page, district_distribution_page, rfm_page])
 pg.run()
