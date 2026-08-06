a2891c6f400dff425cf3bd12720db39ba5fdf471
dulnith-liyanage <dnethmiraliyanage@gmail.com>
2026-08-06 15:33:36 +0530

    Fixed error with subprocess

--- Application/dashboard.py
+++ Application/dashboard.py
@@ -1,6 +1,4 @@
 import streamlit as st
-import subprocess
-
 
 welcome_page = st.Page("welcome.py", title="Welcome")
 sales_performance_page = st.Page("sales_performance.py", title="Sales Performance", icon="📈")
@@ -8,4 +6,3 @@ rfm_page = st.Page("rfm.py", title="Customer Segments", icon="👥")
 chatbot_page = st.Page("bot.py", title = "Insight.AI", icon="🤖")
 pg = st.navigation([welcome_page, sales_performance_page, rfm_page, chatbot_page])
 pg.run()
-subprocess.run(["python3", "./TelegramBot.py"])
