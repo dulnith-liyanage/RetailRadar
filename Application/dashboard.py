5682682484630e67914df6d0e157993a1b7b5472
Thenul <sahant2005@gmail.com>
2026-08-07 14:26:19 +0530

    Changed bot icon

--- Application/dashboard.py
+++ Application/dashboard.py
@@ -3,6 +3,6 @@ import streamlit as st
 welcome_page = st.Page("welcome.py", title="Welcome")
 sales_performance_page = st.Page("sales_performance.py", title="Sales Performance", icon="📈")
 rfm_page = st.Page("rfm.py", title="Customer Segments", icon="👥")
-chatbot_page = st.Page("bot.py", title = "Insight.AI", icon="🤖")
+chatbot_page = st.Page("bot.py", title = "Insight.AI", icon="💡")
 pg = st.navigation([welcome_page, sales_performance_page, rfm_page, chatbot_page])
 pg.run()
